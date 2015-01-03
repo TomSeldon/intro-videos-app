8701423aa196a5f824feb33e8abde2a724da95e6
Tom Seldon <tom@tomseldon.co.uk>
2015-01-03 02:02:35 +0000

    Improve logging

--- index.js
+++ index.js
@@ -4,32 +4,58 @@ var DEFAULT_PORT = 3000;
 
 var fs = require('fs');
 var path = require('path');
+var winston = require('winston');
+var morgan = require('morgan');
 var promisify = require('./lib/promisify');
 var readdir = promisify(fs.readdir);
 var express = require('express');
 var app = express();
 var videoDir = process.env.INTRO_VIDEO_DIR;
 var port = process.env.INTRO_VIDEO_PORT || DEFAULT_PORT;
-var logger = log;
+var accessLogStream = fs.createWriteStream(__dirname + '/access.log', {flags: 'a'});
+var logger = new winston.Logger({
+    transports: [
+        new winston.transports.File({
+            level: 'debug',
+            filename: './logs/info.log',
+            handleExceptions: true,
+            json: false,
+            maxsize: 5242880, //5MB
+            maxFiles: 5,
+            colorize: false
+        }),
+
+        new winston.transports.Console({
+            level: 'debug',
+            handleExceptions: true,
+            json: false,
+            colorize: true
+        })
+    ],
+
+    exitOnError: false
+});
 
 if (!videoDir) {
-    console.error('No video directory specified. Set the env var `INTRO_VIDEO_DIR` to where your videos are located.');
+    logger.error('No video directory specified. Set the env var `INTRO_VIDEO_DIR` to where your videos are located.');
     process.exit(1);
 }
+
+app.use(morgan('combined', {stream: accessLogStream}));
 
 app.route('/')
     .get(function(req, res) {
         getIntroVideos()
             .then(getRandomVideo)
             .then(function(video) {
-                logger('Responding with: ' + video);
+                logger.debug('Responding with: ' + video);
 
                 res.sendFile(video);
             });
     });
 
 app.listen(port, function() {
-    logger('Serving random videos...');
+    logger.debug('Serving random videos...');
 });
 
 function getIntroVideos() {
@@ -41,14 +67,10 @@ function getRandomVideo(videos) {
     var video = videos[index];
     var absPath = path.resolve(videoDir, video);
 
-    logger('Selecting a random video from: ' + videos);
-    logger('Chosen video: ' + absPath);
+    logger.debug('Selecting a random video from: ' + videos);
+    logger.debug('Chosen video: ' + absPath);
 
     return path.resolve(absPath);
-}
-
-function log(message) {
-    console.log(message);
 }
 
 module.exports = app;
